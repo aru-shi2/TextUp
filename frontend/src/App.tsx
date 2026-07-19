3a93f9d62611d14d4bafaee4473a41935e1eee1b
aru-shi2 <190466810+aru-shi2@users.noreply.github.com>
2026-07-19 17:38:12 +0530

    bugs fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,15 @@ import Room from "./Pages/Room"
 import Home from "./Pages/Home"
 import { useEffect, useState } from "react"
 import { nanoid } from "nanoid"
-import dotenv from "dotenv"
 
 function App() {
 
-  dotenv.config()
   const [socket, setsocket] = useState<WebSocket | null>(null)
 
   const [senderId, setsenderId] = useState("")
 
   useEffect(() => {
-    const ws=new WebSocket(process.env.BACKEND_URL as string)
+    const ws=new WebSocket(import.meta.env.BACKEND_URL)
 
     setsocket(ws);
     setsenderId(nanoid(6))
