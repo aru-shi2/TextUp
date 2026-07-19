b3f8d296ac9802ebeefb91d46becc2e63ae00131
aru-shi2 <190466810+aru-shi2@users.noreply.github.com>
2026-07-19 17:53:50 +0530

    bugss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,53 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Room from "./Pages/Room"
-import Home from "./Pages/Home"
-import { useEffect, useState } from "react"
-import { nanoid } from "nanoid"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Room from "./Pages/Room";
+import Home from "./Pages/Home";
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [socket, setsocket] = useState<WebSocket | null>(null);
 
-  const [socket, setsocket] = useState<WebSocket | null>(null)
-
-  const [senderId, setsenderId] = useState("")
+  const [senderId, setsenderId] = useState("");
 
   useEffect(() => {
-    const ws=new WebSocket(import.meta.env.BACKEND_URL)
+    const ws = new WebSocket(import.meta.env.VITE_BACKEND_URL);
+
+    ws.onopen = () => {
+      console.log("Connected");
+    };
+
+    ws.onerror = (e) => {
+      console.log("Error", e);
+    };
+
+    ws.onclose = () => {
+      console.log("Closed");
+    };
 
     setsocket(ws);
-    setsenderId(nanoid(6))
+    setsenderId(nanoid(6));
 
-    return()=>{
+    return () => {
       ws.close();
-    }
-  }, [])
-  
+    };
+  }, []);
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home socket={socket} senderId={senderId}/>}/>
-            <Route path="/:roomId" element={<Room socket={socket} senderId={senderId}/>}/>
+          <Route
+            path="/"
+            element={<Home socket={socket} senderId={senderId} />}
+          />
+          <Route
+            path="/:roomId"
+            element={<Room socket={socket} senderId={senderId} />}
+          />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
